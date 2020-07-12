51ed610f82003fc42c19bc1c28e513e54e688f31
Siscs <siscsti@gmail.com>
2020-07-11 22:16:43 -0300

    Add autenticacao

---
+++ Models/TokenConfig.cs
@@ -0,0 +1,12 @@
+namespace lxwebapijwt.Models
+{
+    public class TokenConfig
+    {
+
+        public string Secret { get; set; }
+        public int ExpiracaoHoras { get; set; }
+        public string Emissor { get; set; }
+        public string ValidoEm { get; set; }
+
+    }
+}
